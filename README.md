5dd152949e678ea75144025e4b64e1f72cfcea3e
ivansimwx <16796516+ivansimwx@users.noreply.github.com>
2024-08-12 18:52:45 +0800

    Setup rspec test

---
+++ README.md
@@ -0,0 +1,4 @@
+setting up rspec
+1. 'rspec --init'
+2. in .rspec file, add '--format documentation' to improve output when executing rspec
+3. create spec version of unit to test e.g. calculator_spec.rb in the spec folder
---
+++ spec/calculator_spec.rb
@@ -0,0 +1,30 @@
+# spec/calculator_spec.rb
+require './lib/calculator'
+
+describe Calculator do
+  describe "#add" do
+    it "returns the sum of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.add(5, 2)).to eql(7)
+    end
+
+    it "returns the sum of more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.add(2, 5, 7)).to eql(14)
+    end
+  end
+
+  describe "#multiply" do
+    it "returns the product of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(5, 2)).to eql(10)
+    end
+  end
+
+  describe "#subtract" do
+    it "returns the difference of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.subtract(5, 2)).to eql(3)
+    end
+  end
+end
